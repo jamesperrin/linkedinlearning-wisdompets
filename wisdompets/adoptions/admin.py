1e6ac4c88b93221b0ce6966378d781cd2fe2982a
James Perrin <countrydawgg@gmail.com>
2020-10-08 12:49:33 -0700

    Completed Section 2 - Working with Django Models and the Admin

--- wisdompets/adoptions/admin.py
+++ wisdompets/adoptions/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from . models import Pet
+
+
+@admin.register(Pet)
+class PetAdmin(admin.ModelAdmin):
+    # pass
+    list_display = ["name", "species", "breed", "age", "sex"]
+
